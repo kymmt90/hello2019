d5ff8244446c3fa1f1d714f76a3f3032bae7d67c
Kohei Yamamoto <kymmt90@gmail.com>
2019-06-30 11:16:58 +0900

    Redis in Action chapter 5.1.2

--- redis-in-action/log.rb
+++ redis-in-action/log.rb
@@ -10,13 +10,37 @@ class Log
     Logger::FATAL => 'fatal'
   }
 
-  def log_recent(name, message, severity: Logger::INFO)
+  def log_recent(name, message, severity: Logger::INFO, conn: connection)
     severity = SEVERITY[severity]
     destination = "recent:#{name}:#{severity}"
 
     connection.pipelined do
-      connection.lpush(destination, "#{Time.new.asctime} #{message}")
-      connection.ltrim(destination, 0, 99)
+      conn.lpush(destination, "#{Time.new.asctime} #{message}")
+      conn.ltrim(destination, 0, 99)
+    end
+  end
+
+  def log_common(name, message, severity: Logger::INFO, timeout: 5)
+    severity = SEVERITY[severity]
+    destination = "common:#{name}:#{severity}"
+
+    start_key = "#{destination}:start"
+
+    connection.watch(start_key) do
+      hour_start = Time.now.to_f
+      existing = connection.get(start_key)
+
+      connection.multi do |multi|
+        if existing && existing < hour_start
+          multi.rename(destination, "#{destination}:last")
+          multi.rename(start_key, "#{destination}:pstart")
+          multi.set(start_key, hour_start)
+        end
+
+        multi.zincrby(destination, 1.0, message)
+
+        log_recent(name, message, severity: severity, conn: multi)
+      end
     end
   end
 
@@ -28,3 +52,4 @@ class Log
 end
 
 Log.new.log_recent('foo', 'yabai', severity: Logger::FATAL)
+Log.new.log_common('foo', 'yabai', severity: Logger::FATAL)
